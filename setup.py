c35c6b24a96ac7adc1a71b6b77966c7fa9986f73
Hasan <85484922+bistcuite@users.noreply.github.com>
2025-02-12 19:50:29 +0330

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='psikit',
+    version='0.1.0',
+    description='A lightweight quantum computing framework for simulating quantum optics experiments, focusing on photonic qubits.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='AgomLabs',
+    author_email='bistcuite@riseup.net',
+    url='https://github.com/your_username/psikit',
+    packages=find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
